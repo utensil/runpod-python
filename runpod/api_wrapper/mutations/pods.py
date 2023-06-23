f30fb429d2e4f1d62c8062c95fbb3578a73cb78b
Utensil <utensilcandel@gmail.com>
2023-06-23 14:39:31 +0000

    Add create_spot_pod

--- runpod/api_wrapper/mutations/pods.py
+++ runpod/api_wrapper/mutations/pods.py
@@ -3,6 +3,7 @@ RunPod | API Wrapper | Mutations | Pods
 """
 # pylint: disable=too-many-arguments, too-many-locals, too-many-branches
 
+# TODO: add deployCost
 
 def generate_pod_deployment_mutation(
         name, image_name, gpu_type_id, cloud_type=None, data_center_id=None, country_code=None,
@@ -83,6 +84,82 @@ def generate_pod_deployment_mutation(
     }}
     """
 
+# TODO: add bidPerGpu
+
+def generate_spot_pod_deployment_mutation(
+        name, image_name, gpu_type_id, cloud_type=None, gpu_count=None,
+        volume_in_gb=None, container_disk_in_gb=None, min_vcpu_count=None,
+        min_memory_in_gb=None, docker_args=None, ports=None, volume_mount_path=None,
+        env=None, support_public_ip=None, min_download=None, min_upload=None,
+        network_volume_id=None, template_id=None, stop_after=None, terminate_after=None):
+    '''
+    Generates a mutation to deploy an interruptable pod.
+    '''
+    input_fields = []
+
+    if cloud_type is not None:
+        input_fields.append(f"cloudType: {cloud_type}")
+    if gpu_count is not None:
+        input_fields.append(f"gpuCount: {gpu_count}")
+    if volume_in_gb is not None:
+        input_fields.append(f"volumeInGb: {volume_in_gb}")
+    if container_disk_in_gb is not None:
+        input_fields.append(f"containerDiskInGb: {container_disk_in_gb}")
+    if min_vcpu_count is not None:
+        input_fields.append(f"minVcpuCount: {min_vcpu_count}")
+    if min_memory_in_gb is not None:
+        input_fields.append(f"minMemoryInGb: {min_memory_in_gb}")
+    if gpu_type_id is not None:
+        input_fields.append(f'gpuTypeId: "{gpu_type_id}"')
+    if name is not None:
+        input_fields.append(f'name: "{name}"')
+    if image_name is not None:
+        input_fields.append(f'imageName: "{image_name}"')
+    if docker_args is not None:
+        input_fields.append(f'dockerArgs: "{docker_args}"')
+    if ports is not None:
+        input_fields.append(f'ports: "{ports}"')
+    if volume_mount_path is not None:
+        input_fields.append(f'volumeMountPath: "{volume_mount_path}"')
+    if env is not None:
+        env_string = ", ".join(
+            [f'{{ key: "{key}", value: "{value}" }}' for key, value in env.items()])
+        input_fields.append(f"env: [{env_string}]")
+    if support_public_ip is not None:
+        input_fields.append(f"supportPublicIp: {support_public_ip}")
+
+    if min_download is not None:
+        input_fields.append(f"minDownload: {min_download}")
+    if min_upload is not None:
+        input_fields.append(f"minUpload: {min_upload}")
+    if network_volume_id is not None:
+        input_fields.append(f"networkVolumeId: {network_volume_id}")
+    if template_id is not None:
+        input_fields.append(f"templateId: {template_id}")
+    if stop_after is not None:
+        input_fields.append(f"stopAfter: {stop_after}")
+    if terminate_after is not None:
+        input_fields.append(f"terminateAfter: {terminate_after}")
+
+    input_string = ", \n".join(input_fields)
+
+    return f"""
+    mutation {{
+      podRentInterruptable(
+        input: {{
+          {input_string}
+        }}
+      ) {{
+        id
+        imageName
+        env
+        machineId
+        machine {{
+          podHostId
+        }}
+      }}
+    }}
+    """
 
 def generate_pod_stop_mutation(pod_id: str) -> str:
     '''
